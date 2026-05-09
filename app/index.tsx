849296ba98fa52495fcef450d550643003fe6e10
rebeca-vital <rebecaosv22@gmail.com>
2026-05-09 16:36:37 -0300

    Atualizando tela de login

--- app/index.tsx
+++ app/index.tsx
@@ -1,15 +1,25 @@
-import { Text, View } from "react-native";
+import { Button, Text, View } from "react-native";
 
 export default function Index() {
   return (
-    <View
+    <View 
       style={{
         flex: 1,
         justifyContent: "center",
         alignItems: "center",
+        backgroundColor: "#f7c6d9",
       }}
-    >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
+>
+      <Text style={{ fontSize:40, fontWeight: "bold", color: "#3b5998",}}>ORKUT</Text>
+      <Text style={{ color:"black", fontSize: 20, marginBottom: 20,}}>Conecte-se aos seus amigos</Text>
+      <View
+      style={{
+        backgroundColor: "white",
+        padding: 100,
+        borderRadius: 10,
+      }}>
+        <Button title="Entrar"></Button>
+      </View>
     </View>
   );
 }
