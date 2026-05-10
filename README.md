1963ea32ad3c13066c7e6e193fcdecd272b36e22
rebeca-vital <rebecaosv22@gmail.com>
2026-05-10 15:47:06 -0300

    finalizada tela inspirada no ORKUT, mas atualização do README

--- README.md
+++ README.md
@@ -1,50 +1,51 @@
-# Welcome to your Expo app 👋
+# Login App - React Native + Expo
 
-This is an [Expo](https://expo.dev) project created with [`create-expo-app`](https://www.npmjs.com/package/create-expo-app).
+Projeto desenvolvido como atividade prática da faculdade utilizando React Native e Expo.
 
-## Get started
+A proposta da atividade era criar uma tela de login contendo:
+- imagem/logo
+- campos de login e senha
+- botão de acesso
+- links de recuperação de conta e criação de conta
 
-1. Install dependencies
+## Sobre o projeto
 
-   ```bash
-   npm install
-   ```
+Desenvolvi uma interface inspirada no antigo Orkut, utilizando uma versão mais moderna e simplificada para dispositivos mobile.
 
-2. Start the app
+## Tecnologias utilizadas
 
-   ```bash
-   npx expo start
-   ```
+- React Native
+- Expo
+- TypeScript
 
-In the output, you'll find options to open the app in a
+## Componentes utilizados
 
-- [development build](https://docs.expo.dev/develop/development-builds/introduction/)
-- [Android emulator](https://docs.expo.dev/workflow/android-studio-emulator/)
-- [iOS simulator](https://docs.expo.dev/workflow/ios-simulator/)
-- [Expo Go](https://expo.dev/go), a limited sandbox for trying out app development with Expo
+- View
+- Text
+- Image
+- TextInput
+- TouchableOpacity
 
-You can start developing by editing the files inside the **app** directory. This project uses [file-based routing](https://docs.expo.dev/router/introduction).
+## Funcionalidades
 
-## Get a fresh project
+- Tela de login estilizada
+- Campos de e-mail e senha
+- Botão de entrada
+- Links de recuperação de conta
+- Layout responsivo básico
 
-When you're ready, run:
+## Aprendizados
+
+Durante o desenvolvimento deste projeto pratiquei:
+- estrutura de componentes
+- estilização com style
+- organização de layout
+- inputs e botões
+- utilização do Expo
+- conceitos básicos de React Native
+
+## Como executar o projeto
 
 ```bash
-npm run reset-project
-```
-
-This command will move the starter code to the **app-example** directory and create a blank **app** directory where you can start developing.
-
-## Learn more
-
-To learn more about developing your project with Expo, look at the following resources:
-
-- [Expo documentation](https://docs.expo.dev/): Learn fundamentals, or go into advanced topics with our [guides](https://docs.expo.dev/guides).
-- [Learn Expo tutorial](https://docs.expo.dev/tutorial/introduction/): Follow a step-by-step tutorial where you'll create a project that runs on Android, iOS, and the web.
-
-## Join the community
-
-Join our community of developers creating universal apps.
-
-- [Expo on GitHub](https://github.com/expo/expo): View our open source platform and contribute.
-- [Discord community](https://chat.expo.dev): Chat with Expo users and ask questions.
+npm install
+npx expo start
--- app/index.tsx
+++ app/index.tsx
@@ -1,4 +1,4 @@
-import { Button, Text, View } from "react-native";
+import { Image, Text, TextInput, TouchableOpacity, View } from "react-native";
 
 export default function Index() {
   return (
@@ -7,18 +7,57 @@ export default function Index() {
         flex: 1,
         justifyContent: "center",
         alignItems: "center",
-        backgroundColor: "#f7c6d9",
+        backgroundColor: "#bfd1e5",
+        padding: 20,
       }}
 >
-      <Text style={{ fontSize:40, fontWeight: "bold", color: "#3b5998",}}>ORKUT</Text>
-      <Text style={{ color:"black", fontSize: 20, marginBottom: 20,}}>Conecte-se aos seus amigos</Text>
+      <Image source={ require("../assets/images/Logo_ORKUT.png") } 
+        style={{ width: 180, height: 60, alignSelf:"center", marginBottom: 20 }} 
+        resizeMode="contain"/>
+      <Text style={{ color:"#555", fontSize: 16, marginBottom: 20, fontWeight: "500",}}>Conecte-se aos seus amigos</Text>
+      
+      // Formulário de login //
       <View
       style={{
         backgroundColor: "white",
-        padding: 100,
-        borderRadius: 10,
-      }}>
-        <Button title="Entrar"></Button>
+        width: "100%",
+        padding: 20,
+        borderRadius: 8,
+        shadowColor: "000",
+        shadowOffset: {width: 0, height: 2},
+        shadowOpacity: 0.1,
+        shadowRadius: 4,
+        elevation: 5,
+        }}>
+        <Text style={{color: "black", fontWeight: "bold", marginBottom: 5}}>
+          Login:
+        </Text>
+        <TextInput 
+        style={{ borderWidth: 1,padding: 12, marginBottom: 15, borderColor: "#ccc", borderRadius: 4 }} 
+        placeholder="Digite seu e-mail" />
+        <Text style={{color: "black", fontWeight: "bold", marginBottom: 5}}>
+          Senha:
+        </Text>
+        <TextInput 
+        style={{ borderWidth: 1,padding: 12, marginBottom: 15, borderColor: "#ccc", borderRadius: 4 }} 
+        placeholder="Digite sua senha" secureTextEntry />
+        <TouchableOpacity
+        style={{ backgroundColor: "#edeff4", padding: 10, borderRadius: 4, borderWidth: 1, 
+        borderColor: "#abc", alignItems: "center" }}>
+          <Text style={{color: "#3b5998", fontWeight: "bold"}}
+          >Entrar</Text>
+        </TouchableOpacity>
+      </View>
+      //recuperação de senha  e criar conta //
+      <View 
+      style={{ marginTop: 20, alignItems: "center" }}>
+        <TouchableOpacity>
+          <Text style={{color: "#3b5998", fontSize: 13, textDecorationLine: "underline"}}>Não consegue acessar sua conta?</Text>
+          </TouchableOpacity>
+          <Text style={{marginVertical: 15, color: "#999"}}>ou</Text>
+          <TouchableOpacity style={{marginTop: 5}}>
+            <Text style={{color: "#f58220", fontWeight: "bold", fontSize: 16}}>CRIAR CONTA</Text>
+          </TouchableOpacity>
       </View>
     </View>
   );
